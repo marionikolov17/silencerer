4bfda37247acbd45a87b0ba43dc78b6edd5afe96
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-03-19 21:45:14 +0200

    Create: Project Viewer: Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import AddedMedia from '@/components/AddedMedia';
+import Header from '@/components/Header';
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen relative flex flex-col overflow-x-hidden">
       <div className="w-full h-[700px] bg-gray-100 flex border-b border-gray-200">
         <AddedMedia />
+        <div className="grow flex flex-col">
+          <Header />
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { PiArrowBendUpLeftBold, PiArrowBendUpRightBold } from "react-icons/pi";
+
+export default function Header() {
+    return (
+        <div className="w-full h-16 flex items-center justify-between px-4">
+            <div className="flex items-center gap-x-2">
+                <h1 className="text-lg font-bold">Silencerer.com</h1>
+                <button
+                    className="ms-8 p-2 rounded-full cursor-pointer hover:bg-gray-200"
+                    title="Undo"
+                >
+                    <PiArrowBendUpLeftBold className="text-xl text-gray-500" />
+                </button>
+                <button
+                    className="ms-2 p-2 rounded-full cursor-pointer hover:bg-gray-200"
+                    title="Redo"
+                >
+                    <PiArrowBendUpRightBold className="text-xl text-gray-500" />
+                </button>
+            </div>
+            <div className="flex items-center gap-x-2">
+                <button
+                    className="bg-orange-500 text-white px-8 py-2 rounded-lg cursor-pointer hover:bg-orange-600 transition-all duration-300"
+                    title="Export Media"
+                >
+                    Export
+                </button>
+                <button
+                    className="bg-blue-500 text-white px-8 py-2 rounded-lg cursor-pointer hover:bg-blue-600 transition-all duration-300 disabled:bg-blue-300 disabled:cursor-not-allowed disabled:hover:bg-blue-300"
+                    title="Save Project (disabled)"
+                    disabled
+                >
+                    Save Project
+                </button>
+            </div>
+        </div>
+    )
+}

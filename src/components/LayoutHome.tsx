9f26feb538866b7070d481f2c8e38737bf4747c8
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-05-27 13:23:55 +0300

    Fix: VideoPreview: component was holding information and state that does not belong to it

--- src/components/LayoutHome.tsx
+++ src/components/LayoutHome.tsx
@@ -5,6 +5,7 @@ import AddedMedia from './AddedMedia';
 import AudioPlayer from './AudioPlayer';
 import Header from './Header';
 import VideoPreview from './VideoPreview';
+import { IoSettingsOutline } from 'react-icons/io5';
 
 export default function LayoutHome() {
   const [isMobileMediaOpened, setIsMobileMediaOpened] = useState(false);
@@ -21,7 +22,27 @@ export default function LayoutHome() {
           />
           <div className="grow flex flex-col w-full sm:w-auto">
             <Header />
-            <VideoPreview setIsMobileMediaOpened={setIsMobileMediaOpened} />
+            <VideoPreview />
+            <div className="w-full py-8 flex items-center justify-center">
+              <div className="h-12 bg-white rounded-lg flex items-center gap-x-4 px-4 overflow-hidden">
+                <p className="text-sm font-medium">Landscape (16:9)</p>
+                <button
+                  className="cursor-pointer flex gap-x-1 items-center hover:bg-gray-100 py-1.5 rounded-lg px-2"
+                  title="Algorithm Settings"
+                >
+                  <IoSettingsOutline className="text-lg" />
+                  <p className="text-sm font-medium">Settings</p>
+                </button>
+              </div>
+            </div>
+            <div className="w-full flex lg:hidden items-center justify-center pb-8">
+              <button
+                className="text-sm"
+                onClick={() => setIsMobileMediaOpened(true)}
+              >
+                Change Media
+              </button>
+            </div>
           </div>
         </div>
         <div className="w-full grow flex flex-col justify-end">
--- src/components/VideoPreview.tsx
+++ src/components/VideoPreview.tsx
@@ -1,38 +1,11 @@
 'use client';
 
-import { Dispatch, SetStateAction } from 'react';
-import { IoSettingsOutline } from 'react-icons/io5';
-
-interface InputProps {
-  setIsMobileMediaOpened: Dispatch<SetStateAction<boolean>>;
-}
-
-export default function VideoPreview({ setIsMobileMediaOpened }: InputProps) {
+export default function VideoPreview() {
   return (
     <div className="w-full grow flex flex-col items-center overflow-hidden">
       <div className="w-full grow flex items-center justify-center py-8 px-8 overflow-hidden">
         {/* Place Video here */}
         <div className="bg-black w-full min-h-[250px] sm:w-[600px] sm:h-[338px] 2xl:w-[800px] 2xl:h-[450px]"></div>
-      </div>
-      <div className="w-full py-8 flex items-center justify-center">
-        <div className="h-12 bg-white rounded-lg flex items-center gap-x-4 px-4 overflow-hidden">
-          <p className="text-sm font-medium">Landscape (16:9)</p>
-          <button
-            className="cursor-pointer flex gap-x-1 items-center hover:bg-gray-100 py-1.5 rounded-lg px-2"
-            title="Algorithm Settings"
-          >
-            <IoSettingsOutline className="text-lg" />
-            <p className="text-sm font-medium">Settings</p>
-          </button>
-        </div>
-      </div>
-      <div className="w-full flex lg:hidden items-center justify-center pb-8">
-        <button
-          className="text-sm"
-          onClick={() => setIsMobileMediaOpened(true)}
-        >
-          Change Media
-        </button>
       </div>
     </div>
   );

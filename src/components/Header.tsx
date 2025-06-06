0961b409f78392127621a74ceb43c203018d1ec2
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2025-06-06 17:17:35 +0300

    Replace: undo/redo buttons with github and linkedin links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import Link from 'next/link';
 import { PiArrowBendUpLeftBold, PiArrowBendUpRightBold } from 'react-icons/pi';
+import { GithubSvg, LinkedInSvg } from './constants/tech-icons';
 interface InputProps {
   openExportWindow: () => void;
 }
@@ -11,17 +13,29 @@ export default function Header({ openExportWindow }: InputProps) {
       <div className="flex items-center gap-x-2">
         <h1 className="text-base sm:text-lg font-bold">Silencerer.com</h1>
         <button
-          className="ms-4sm:ms-8 p-2 rounded-full cursor-pointer hover:bg-gray-200"
+          className="ms-4 sm:ms-8 p-2 rounded-full cursor-pointer hover:bg-gray-200 hidden"
           title="Undo"
         >
           <PiArrowBendUpLeftBold className="text-xl text-gray-500" />
         </button>
         <button
-          className="sm:ms-2 p-2 rounded-full cursor-pointer hover:bg-gray-200"
+          className="sm:ms-2 p-2 rounded-full cursor-pointer hover:bg-gray-200 hidden"
           title="Redo"
         >
           <PiArrowBendUpRightBold className="text-xl text-gray-500" />
         </button>
+        <Link
+          href="https://github.com/marionikolov17/silencerer"
+          className="ms-2"
+        >
+          <GithubSvg className="w-6 h-6 text-gray-300 hover:text-blue-500 transition-all duration-300" />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/marionikolovdev/"
+          className="ms-1"
+        >
+          <LinkedInSvg className="w-6 h-6 text-gray-300 hover:text-blue-500 transition-all duration-300" />
+        </Link>
       </div>
       <div className="flex items-center gap-x-2">
         <button
